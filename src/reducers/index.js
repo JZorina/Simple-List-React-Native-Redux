6e98bc6ff381197579401b5268799c3937e0c06e
yulia <yuliazo@ac.sce.ac.il>
2019-10-15 02:38:26 +0300

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
+export default combineReducers({
+    libraries:LibraryReducer,
+    selectLibraryId:SelectionReducer
+});
